69fe73f7bb3043a871ff1bbcb3f8af8dc4747491
Chris Capper <55027395+chris-capper@users.noreply.github.com>
2020-06-02 15:30:26 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function hamburgerMenu() {
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
