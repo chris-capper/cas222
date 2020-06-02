f9f1bf1b2fff5eb9477724c21ec128046fe33e46
Chris Capper <55027395+chris-capper@users.noreply.github.com>
2020-06-02 15:33:47 -0700

    Add files via upload

--- ace/site_files/faq/faq.html.php
+++ ace/site_files/faq/faq.html.php
@@ -1,1 +1,77 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../styles/reset.css">
+        <link rel="stylesheet" href="../styles/helper.css">
+        <link rel="stylesheet" href="../styles/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="../scripts/newscript.js"></script>
+        <title>Ace in the Hole | FAQ</title>
+    </head>
+    <body>
+        <?php include '../includes/nav.inc.html.php'; ?>
+        <?php include '../includes/header.inc.html.php'; ?>
+        <main>
+        <img src="../images/ace17.jpg" alt="people running marathon"></img>
+            <h2>Frequently Asked Questions</h2>
+            <p>We know you must have questions, but if you don't see your question answered here please
+                <a href="../about/index.php">send us a comment here!</a></p>
+            <h3>What are the Rules?</h3>
+                <p>We currently adhere to the USAT Rules for Triathlon & Duathlon. 
+                    Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY 
+                    NOT allowed during the bike for obvious safety reasons.</p>
+            <h3>Can I use a personal music device while cycling?</h3>
+                <p>Absolutely no music devices are allowed during the bike segment for obvious safety 
+                    risks and will result in immediate disqualification.</p>
+            <h3>Can I use a personal music device while running?</h3>
+                <p> During any running segments, we prefer for athletes to NOT use music devices to 
+                    maximize safety, assure a fair competitive environment and assure athletes can hear 
+                    all course marshal instructions. With that said, we also understand that many athletes 
+                    rely on music to help endure the challenge of running. To accommodate the needs of our 
+                    athletes while still assuring maximum safety and a fair, competitive environment, here 
+                    is the arrangement that we can accommodate.</p>
+                <p>If you chose to use a personal music device, you must always keep one ear open for 
+                    instructions so only one ear-bud can be used at any time. In addition, if you opt to 
+                    use music, you will not be eligible for awards, points and rankings. So as an athlete, 
+                    you have to make a choice - music or awards/points/rankings.</p>
+                <p>If you are using a personal music devise with two earbuds, you will be immediately 
+                    disqualified. Trust us, we don't want to have to enforce a disqualification but in 
+                    order to assure the safety of all athletes, this is a very important rule.</p>
+                <p>We also request that if you chose to use a personal music device that you keep the 
+                    volume to a minimum to assure all instructions can be heard.</p>
+            <h3>Do I need to wear a wetsuit?</h3>
+                <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others 
+                    will choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+            <h3>Do I have to use a road or racing bike?</h3>
+                <p>No. We welcome any type of bike as long as it is functioning properly with brakes 
+                    and endcaps at the end of your handlebars.</p>
+            <h2>What to Bring</h2>
+                <p>Watch the weather closely. The show goes on no matter what the weather is doing.</p>
+                <p>You must wear your chip timing piece during the entire event. You will be given a band 
+                    that will hold your chip timing piece around your ankle throughout the entire event. 
+                    Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you 
+                    start the race and be sure to step over the timing mats after each segment of the race. If 
+                    you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, 
+                    you will have a very difficult time getting off your wet suit.</p>
+            <h3>Swim:</h3>
+            <p>Wetsuits are optional for the swim but will provide buoyancy and warmth. However, 
+                many people opt for no wetsuit for a triathlon so no worries. We will provide you with a 
+                swim cap but you will want to bring your own goggles.</p>
+            <h3>Bike:</h3>
+            <p>A biking helmet is mandatory. You will also receive 2 stickers in your package with your 
+                race number on them. The small sticker should go on the front of your helmet. The bigger 
+                sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For safety 
+                reasons, be sure to have end-caps on the end of your handle-bars.</p>
+            <h3>Run:</h3>
+            <p>You must finish the race with your bib number on the front of you. Some people choose to pin 
+                it on at the beginning of the race and have it on for the whole event so they don't have to 
+                worry about it. Others pin it on a singlet that they put on once they finish the swim before 
+                they head out for the bike. Others use an elastic racing strap that they pin their bib number 
+                to and then quickly strap it on before they leave for the run. Use whatever option feels best 
+                for you.</p>
+            <p>Remember to bring a change of clothing so you can enjoy the post-event festivities.</p>
+        </main>
+        <?php include '../includes/footer.inc.html.php' ?>
+    </body>
+</html>
---
+++ ace/site_files/faq/index.php
@@ -0,0 +1,4 @@
+<?php 
+    include '../includes/db.inc.html.php'; 
+    include 'faq.html.php';
+?>
