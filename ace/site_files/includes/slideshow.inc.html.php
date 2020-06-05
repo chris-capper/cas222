5a6fa282b83886fecf1499cd98673b245a443ef1
Chris Capper <55027395+chris-capper@users.noreply.github.com>
2020-06-05 13:25:00 -0700

    Add files via upload

--- ace/site_files/includes/slideshow.inc.html.php
+++ ace/site_files/includes/slideshow.inc.html.php
@@ -5,91 +5,91 @@
 <!-- Full-width images with number and caption text -->
     <div class="mySlides fade">
         <div class="numbertext">1 / 15</div>
-            <img src="./images/cropped-images/ace1-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace1-cropped.jpg" style="width:100%" alt="Competitors run through water">
+        <div class="text">Splash and Dash!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">2 / 15</div>
-            <img src="./images/cropped-images/ace2-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace2-cropped.jpg" style="width:100%" alt="all ages run">
+        <div class="text">Fun for everyone!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">3 / 15</div>
-            <img src="./images/cropped-images/ace3-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace3-cropped.jpg" style="width:100%" alt="bicyclists celebrating">
+        <div class="text">Come and push yourself!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">4 / 15</div>
-            <img src="./images/cropped-images/ace4-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace4-cropped.jpg" style="width:100%" alt="swimmers running into ocean">
+        <div class="text">Swimmer's start!</div>
     </div>
     <div class="mySlides fade">
         <div class="numbertext">5 / 15</div>
-            <img src="./images/cropped-images/ace5-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace5-cropped.jpg" style="width:100%" alt="biking into the sun">
+        <div class="text">Bikes bikes bikes!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">6 / 15</div>
-            <img src="./images/cropped-images/ace6-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace6-cropped.jpg" style="width:100%" alt="finish line refreshments">
+        <div class="text">Enjoy some refreshments at the finish line!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">7 / 15</div>
-            <img src="./images/cropped-images/ace8-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace8-cropped.jpg" style="width:100%" alt="smiling woman at marathon">
+        <div class="text">Come compete in a marathon!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">8 / 15</div>
-            <img src="./images/cropped-images/ace9-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace9-cropped.jpg" style="width:100%" alt="wheelchair race">
+        <div class="text">Everyone is welcome!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">9 / 15</div>
-            <img src="./images/cropped-images/ace10-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace10-cropped.jpg" style="width:100%" alt="finish line">
+        <div class="text">See you at the finish line!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">10 / 15</div>
-            <img src="./images/cropped-images/ace11-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace11-cropped.jpg" style="width:100%" alt="sneakers">
+        <div class="text">Come run with us!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">11 / 15</div>
-            <img src="./images/cropped-images/ace13-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace13-cropped.jpg" style="width:100%" alt="10k race">
+        <div class="text">Try our 10k!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">12 / 15</div>
-            <img src="./images/cropped-images/ace14-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace14-cropped.jpg" style="width:100%" alt="sprinters">
+        <div class="text">Sprint your way to victory!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">13 / 15</div>
-            <img src="./images/cropped-images/ace16-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace16-cropped.jpg" style="width:100%" alt="end of marathon">
+        <div class="text">Come cheer everyone on!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">14 / 15</div>
-            <img src="./images/cropped-images/ace17-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace17-cropped.jpg" style="width:100%" alt="large marathon">
+        <div class="text">Big or small, our marathon is always a ton of fun!</div>
     </div>
 
     <div class="mySlides fade">
         <div class="numbertext">15 / 15</div>
-            <img src="./images/cropped-images/ace18-cropped.jpg" style="width:100%">
-        <div class="text">Caption</div>
+            <img src="./images/cropped-images/ace18-cropped.jpg" style="width:100%" alt="explore the city">
+        <div class="text">Explore the city while you compete!</div>
     </div>
 
 <!-- Next and previous buttons -->
